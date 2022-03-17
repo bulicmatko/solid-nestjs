19bc332ce09d854419fdebacbcaefafb25d65083
Matko Bulic <bulicmatko@gmail.com>
2022-03-17 16:43:21 +0100

    remove validation on default user interface find many

--- src/user-interface/inputs/find-many-where-string.input.ts
+++ src/user-interface/inputs/find-many-where-string.input.ts
@@ -1,52 +1,38 @@
 import { Field, InputType } from "@nestjs/graphql";
-import { IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+import { IsOptional, IsString } from "class-validator";
 
 @InputType()
 export class FindManyWhereString {
-  @MaxLength(100)
-  @MinLength(3)
   @IsString()
   @IsOptional()
   @Field(() => String, { nullable: true })
   readonly equals?: string;
 
-  @MaxLength(100)
-  @MinLength(3)
   @IsString()
   @IsOptional()
   @Field(() => String, { nullable: true })
   readonly not?: string;
 
-  @MaxLength(100)
-  @MinLength(3)
   @IsString()
   @IsOptional()
   @Field(() => String, { nullable: true })
   readonly contains?: string;
 
-  @MaxLength(100)
-  @MinLength(3)
   @IsString()
   @IsOptional()
   @Field(() => String, { nullable: true })
   readonly startsWith?: string;
 
-  @MaxLength(100)
-  @MinLength(3)
   @IsString()
   @IsOptional()
   @Field(() => String, { nullable: true })
   readonly endsWith?: string;
 
-  @MaxLength(100, { each: true })
-  @MinLength(3, { each: true })
   @IsString({ each: true })
   @IsOptional()
   @Field(() => [String], { nullable: true })
   readonly in?: string[];
 
-  @MaxLength(100, { each: true })
-  @MinLength(3, { each: true })
   @IsString({ each: true })
   @IsOptional()
   @Field(() => [String], { nullable: true })
