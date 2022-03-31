df0065b97d762a85ee3fcf6b82cab85c69f8f2b2
Matko Bulic <bulicmatko@gmail.com>
2022-03-31 14:43:12 +0200

    rename filters folder

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestExpressApplication as NestApp } from "@nestjs/platform-express";
 import { useContainer } from "class-validator";
 
 // import { GlobalValidationPipe } from "./pipes/global-validation.pipe";
-import { GlobalExceptionFilter } from "./filter/global-exception.filter";
+import { GlobalExceptionFilter } from "./filters/global-exception.filter";
 
 import { AppModule } from "./app.module";
 
